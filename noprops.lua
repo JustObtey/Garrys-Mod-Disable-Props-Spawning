d7d7c9fd5fcd21190e85b245fa317103e98fbd42
xx_Obtey_xx <95309102+xxObteyxx@users.noreply.github.com>
2022-10-25 18:42:04 +0200

    Delete noprops.lua

--- noprops.lua
+++
@@ -1,8 +0,0 @@
-hook.Add("PlayerSpawnProp", "RestrictSpawningProps", function(ply)
--- You can change "Staff" by any other job name, make sure it's the name and not the TEAM name
-    if team.GetName(ply:Team()) == "Staff" then
-        return true
-    else
-        return false
-    end
-end)
