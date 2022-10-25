5aa221c7b6f54b6c6c9811fd2b8b218b579b2fb1
xx_Obtey_xx <95309102+xxObteyxx@users.noreply.github.com>
2022-10-25 23:49:03 +0200

    Update noprops.lua

--- noprops/lua/autorun/server/noprops.lua
+++ noprops/lua/autorun/server/noprops.lua
@@ -1,8 +1,14 @@
+-- Put the jobs who can spawn props there
+NoProps_Jobs  = {
+    ["Staff"] = true,
+    ["Name Job"] = false
+}
+
 hook.Add("PlayerSpawnProp", "RestrictSpawningProps", function(ply)
 -- You can change "Staff" by any other job name, make sure it's the name and not the TEAM name
-    if team.GetName(ply:Team()) == "Staff" then
+    if NoProps_Jobs[team.GetName(ply:Team())] then
         return true
     else
         return false
     end
-end)
+end)```
